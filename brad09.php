4a8d29f9426ab2c0222a30e173627a0f3c0fd664
bradchao <brad@brad.tw>
2024-10-23 14:59:19 +0800

    10231459

--- brad09.php
+++ brad09.php
@@ -1,5 +1,5 @@
 <?php
-    $x = $y = $r = '';
+    $x = $y = $r = $op = '';
     if (isset($_GET['x'])){
         $x = $_GET['x']; $y = $_GET['y'];
         $op = $_GET['op'];
@@ -21,10 +21,10 @@
 <form action="brad09.php">
     <input name="x" value="<?php echo $x; ?>">
     <select name="op">
-        <option value="1">+</option>
-        <option value="2">-</option>
-        <option value="3" selected>x</option>
-        <option value="4">/</option>
+        <option value="1" <?php echo $op=='1'?'selected':''; ?>>+</option>
+        <option value="2" <?php echo $op=='2'?'selected':''; ?>>-</option>
+        <option value="3" <?php echo $op=='3'?'selected':''; ?>>x</option>
+        <option value="4" <?php echo $op=='4'?'selected':''; ?>>/</option>
     </select>
     <input name="y" value="<?php echo $y; ?>">
     <input type="submit" value="=" />

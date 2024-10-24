ab193c9a5f2ec99d8252a39953237396662373ad
bradchao <brad@brad.tw>
2024-10-24 09:12:55 +0800

    10240912

--- brad99.php
+++ brad99.php
@@ -1,29 +1,15 @@
 <table border="1" width="100%">
     <tr>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
-        <td>
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-            2 x 1 = 2<br />
-        </td>
+        <?php
+            for ($j = 2; $j <= 5; $j++) {
+                echo '<td>';
+                for ($i = 1; $i <= 9; $i++) {
+                    $r = $j * $i;
+                    echo "$j x $i = $r<br />";
+                }
+                echo "</td>";
+            }
+        ?>
     </tr>
 
     <tr>

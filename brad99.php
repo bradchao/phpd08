3e2731af46279a96a8b429f92605c09610738da0
bradchao <brad@brad.tw>
2024-10-24 09:52:04 +0800

    10240952

--- brad99.php
+++ brad99.php
@@ -1,9 +1,31 @@
+<?php
+    $start = 2; $rows = 2; $cols = 4;
+    if (isset($_GET['start'])){
+        $start = $_GET['start'];
+        $rows = $_GET['rows'];
+        $cols = $_GET['cols'];
+    }
+
+    define("ROWS", $rows);
+    define("COLS", $cols);
+    define("START", $start);
+?>
+
+<h1>Brad Big Company</h1>
+<hr />
+<form action="brad99.php">
+    Start: <input type="number" name="start" />
+    rows: <input type="number" name="rows" />
+    cols: <input type="number" name="cols" />
+    <input type="submit" value="Change" />
+</form>
+<hr />
 <table border="1" width="100%">
     <?php
-        for ($k = 0; $k < 2; $k++) {
+        for ($k = 0; $k < ROWS; $k++) {
             echo '<tr>';
-            for ($j = 2; $j <= 5; $j++) {
-                $newj = $j + $k * 4;
+            for ($j = START; $j < START+COLS; $j++) {
+                $newj = $j + $k * COLS;
                 echo '<td>';
                 for ($i = 1; $i <= 9; $i++) {
                     $r = $newj * $i;

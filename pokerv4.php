197f5182e51a7656cc192eba2c5d424b3385c813
bradchao <brad@brad.tw>
2024-10-24 15:04:00 +0800

    10241503

--- pokerv4.php
+++ pokerv4.php
@@ -1,8 +1,10 @@
 <?php
     $poker = range(0,51);
     shuffle( $poker );
-    
+
+    $players = [[],[],[],[]];    
     foreach ($poker as $k => $v) {
-        echo "$k : $v<br />";
-    }    
+        $players[$k%4][(int)($k/4)] = $v;        
+    }
+
 ?>

488c9e78449fa1734dd92ced16295aae96f27fd8
bradchao <brad@brad.tw>
2024-10-29 15:03:46 +0800

    10291503

--- brad38.php
+++ brad38.php
@@ -8,6 +8,10 @@
         'root','', 'brad');
     $mysqli->set_charset('utf8');
 
+    $mysqli->query('DELETE FROM gift');
+    $mysqli->query('ALTER TABLE gift AUTO_INCREMENT = 1');
+
+
     foreach ($datas as $row) {
         $name = $row->Name;
         $feature = $row->Feature;

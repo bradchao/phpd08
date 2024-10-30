99a3a3766e7a7ba1cfeda601e62c2d8f1305b814
bradchao <brad@brad.tw>
2024-10-30 09:44:36 +0800

    10300944

--- brad44.php
+++ brad44.php
@@ -3,11 +3,11 @@
     'root','', 'brad');
     $mysqli->set_charset('utf8');   
 
-    $sql = 'SELECT id,name,feature,addr,tel FROM gift';
+    $sql = 'SELECT id,name,feature,addr,tel,picurl FROM gift';
     $stmt = $mysqli->prepare($sql);
     if ($stmt->execute()){
         $stmt->store_result();
-        $stmt->bind_result($id,$name,$feature,$addr,$tel);
+        $stmt->bind_result($id,$name,$feature,$addr,$tel,$picurl);
 
 ?>
 <table border="1" width="100%">
@@ -17,6 +17,7 @@
         <th>Feature</th>
         <th>Address</th>
         <th>Tel</th>
+        <th>Picture</th>
     </tr>
     <?php
         while($stmt->fetch()){
@@ -26,6 +27,7 @@
             echo "<td>{$feature}</td>";
             echo "<td>{$addr}</td>";
             echo "<td>{$tel}</td>";
+            echo "<td><img src='{$picurl}' width='160px' height='90px'></td>";
             echo '</tr>';
         }
     ?>

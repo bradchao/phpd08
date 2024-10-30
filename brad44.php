5c90a67fe1f599f9ee1163a9f7ce2afdf5d2418a
bradchao <brad@brad.tw>
2024-10-30 10:48:50 +0800

    10301048

--- brad44.php
+++ brad44.php
@@ -2,21 +2,40 @@
     $rpp = 4;  // rows per page
     $page = 1;
     if (isset($_GET["page"])) { $page = $_GET["page"];}
-    
+
     $start = ($page -1) * $rpp ;
+    $prev = $page == 1?1: $page -1;
+    $next = $page + 1;
 
     $mysqli = new mysqli('localhost',
     'root','', 'brad');
     $mysqli->set_charset('utf8');   
 
-    $sql = 'SELECT id,name,feature,addr,tel,picurl FROM gift LIMIT ?, ?';
-    $stmt = $mysqli->prepare($sql);
-    $stmt->bind_param('ii', $start, $rpp);
+    
+    $sql = 'SELECT id,name,feature,addr,tel,picurl FROM gift';
+    if (isset($_GET['key']) && strlen($_GET['key'])>0) {
+        $key = $_GET['key'];
+        $skey = "%{$key}%";
+        $sql .= ' WHERE name LIKE ? OR feature LIKE ? OR addr LIKE ?';
+        $stmt = $mysqli->prepare($sql);
+        $stmt->bind_param('sss', $skey, $skey, $skey);
+    }else{
+        $stmt = $mysqli->prepare($sql);
+    }
+
     if ($stmt->execute()){
         $stmt->store_result();
         $stmt->bind_result($id,$name,$feature,$addr,$tel,$picurl);
 
 ?>
+<form>
+    Keyword: <input name="key" />
+    <input type="submit" value="Search" />
+</form>
+<hr />
+<a href="?page=<?php echo $prev; ?>">Prev</a> | 
+<a href="?page=<?php echo $next; ?>">Next</a>
+<hr />
 <table border="1" width="100%">
     <tr>
         <th>ID</th>

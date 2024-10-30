8bfc1a54875e79a35a76d9ff001d917787833860
bradchao <brad@brad.tw>
2024-10-30 10:03:16 +0800

    10301003

--- brad44.php
+++ brad44.php
@@ -1,10 +1,17 @@
 <?php
+    $rpp = 4;  // rows per page
+    $page = 1;
+    if (isset($_GET["page"])) { $page = $_GET["page"];}
+    
+    $start = ($page -1) * $rpp ;
+
     $mysqli = new mysqli('localhost',
     'root','', 'brad');
     $mysqli->set_charset('utf8');   
 
-    $sql = 'SELECT id,name,feature,addr,tel,picurl FROM gift';
+    $sql = 'SELECT id,name,feature,addr,tel,picurl FROM gift LIMIT ?, ?';
     $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param('ii', $start, $rpp);
     if ($stmt->execute()){
         $stmt->store_result();
         $stmt->bind_result($id,$name,$feature,$addr,$tel,$picurl);

2acb1a2a97db50ad019c6586987e49965b93c048
bradchao <brad@brad.tw>
2024-11-04 14:43:47 +0800

    11041443

--- brad51.php
+++ brad51.php
@@ -16,8 +16,43 @@
         SELECT OrderID FROM orders
         WHERE EmployeeID = 4
     )
-
-
     */
 
+    $mysqli = new mysqli('localhost',
+    'root','', 'northwind');
+    $mysqli->set_charset('utf8'); 
+
+    $sql = 'SELECT e.EmployeeID id, e.LastName ename, sum(od.UnitPrice * od.Quantity) total
+            FROM orders o
+            JOIN orderdetails od ON (o.OrderID = od.OrderID)
+            JOIN employees e ON (o.EmployeeID = e.EmployeeID)
+            GROUP BY e.EmployeeID
+            ORDER BY total DESC';
+
+    $stmt = $mysqli->prepare($sql);
+    $stmt->execute();
+    $stmt->store_result();
+    $stmt->bind_result($id, $ename, $total);
+
 ?>
+<table border="1">
+    <tr>
+        <th>Rank</th>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Sales</th>
+    </tr>
+    <?php
+        $rand = 1;
+        while ($stmt->fetch()){
+            echo '<tr>';
+            echo "<td>{$rand}</td>";
+            echo "<td>{$id}</td>";
+            echo "<td>{$ename}</td>";
+            echo "<td>{$total}</td>";
+            echo '</tr>';
+            $rand++;
+        }
+    ?>
+
+</table>
